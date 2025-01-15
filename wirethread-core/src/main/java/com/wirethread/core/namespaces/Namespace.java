1d328fa3f4507f003e1639aa9f1ab8b482dbe8bd
Flangrys <Giorgisprieto@outlook.com.ar>
2025-01-15 10:46:57 -0300

    chore(Namespace): Adding documentation to the Namespace impl.

--- wirethread-core/src/main/java/com/wirethread/core/namespaces/Namespace.java
+++ wirethread-core/src/main/java/com/wirethread/core/namespaces/Namespace.java
@@ -1,23 +1,29 @@
 package com.wirethread.core.namespaces;
 
-import com.wirethread.core.plugins.Pluggable;
+import com.wirethread.plugin.Pluggable;
 import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.regex.Pattern;
 
 
+/**
+ * This record represents a Minecraft resource locator.
+ *
+ * @param domain The scope owner of this namespace, like 'minecraft' or 'my-plugin'.
+ * @param path   The location of this specific resource.
+ */
 public record Namespace(String domain, String path) {
 
     public static final String MINECRAFT = "minecraft";
     public static final String WIRETHREAD = "wirethread";
 
-    public static final String ALLOWED_CHARACTERS = "[a-z0-9_-]{1,62}";
-    public static final String ALLOWED_GROUPS = "^(" + ALLOWED_CHARACTERS + ")(/" + ALLOWED_CHARACTERS + ")*(\\." + ALLOWED_CHARACTERS + ")*$";
-    public static final String DEFAULT_SEPARATOR = ":";
+    private static final String ALLOWED_CHARACTERS = "[a-z0-9_-]{1,62}";
+    private static final String ALLOWED_GROUPS = "^(" + ALLOWED_CHARACTERS + ")(/" + ALLOWED_CHARACTERS + ")*(\\." + ALLOWED_CHARACTERS + ")*$";
+    private static final String DEFAULT_SEPARATOR = ":";
 
-    public static final Pattern ALLOWED_CHARACTERS_PATTERN = Pattern.compile("(" + ALLOWED_CHARACTERS + ")");
-    public static final Pattern ALLOWED_GROUPS_PATTERN = Pattern.compile(ALLOWED_GROUPS);
+    private static final Pattern ALLOWED_CHARACTERS_PATTERN = Pattern.compile("(" + ALLOWED_CHARACTERS + ")");
+    private static final Pattern ALLOWED_GROUPS_PATTERN = Pattern.compile(ALLOWED_GROUPS);
 
     /**
      * Create a new {@code Namespace} from a domain and a path.
@@ -39,20 +45,43 @@ public record Namespace(String domain, String path) {
         }
     }
 
+    /**
+     * Asserts if a namespace domain is valid.
+     *
+     * @param domain A string-like namespace domain.
+     * @return True when the namespace domain were valid. False otherwise.
+     */
     public static boolean assertValidDomain(@NotNull String domain) {
         return !domain.isBlank() && ALLOWED_CHARACTERS_PATTERN.matcher(domain).matches();
     }
 
+    /**
+     * Asserts if a namespace path is valid.
+     *
+     * @param path A string-like namespace path.
+     * @return True when the namespace path were valid. False otherwise.
+     */
     public static boolean assertValidPath(@NotNull String path) {
         return !path.isBlank() && (
-            ALLOWED_GROUPS_PATTERN.matcher(path).matches() || ALLOWED_CHARACTERS_PATTERN.matcher(path).matches()
+                ALLOWED_GROUPS_PATTERN.matcher(path).matches() || ALLOWED_CHARACTERS_PATTERN.matcher(path).matches()
         );
     }
 
-    public String[] getModule() { return null; }
+    /**
+     * Retrieves the namespace module base on the '/' separator.
+     * @return null.
+     */
+    @ApiStatus.Experimental
+    public String[] getModule() {
+        throw new UnsupportedOperationException("Not implemented yet.");
+    }
 
+    /**
+     * Retrieves the namespace
+     * @return null.
+     */
     public String[] getPath() {
-        return null;
+        throw new UnsupportedOperationException("Not implemented yet.");
     }
 
     /**
@@ -67,14 +96,16 @@ public record Namespace(String domain, String path) {
     public static Namespace from(@NotNull String namespace) {
         String[] components = namespace.split(DEFAULT_SEPARATOR, 0);
 
-        if (components.length > 2) throw new IllegalArgumentException("Malformed namespace, only one separator is allowed. <domain>:<path>");
+        if (components.length > 2) {
+            throw new IllegalArgumentException("Malformed namespace, only one separator is allowed. <domain>:<path>");
+        }
 
         return new Namespace(components[0], components[1]);
     }
 
     @NotNull
     public static Namespace fromMinecraft(String path) {
-        return from(MINECRAFT + "+" +  path);
+        return from(MINECRAFT + "+" + path);
     }
 
     @NotNull
